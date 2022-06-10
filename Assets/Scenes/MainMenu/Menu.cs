76aee62f58ed3e9c03412240d1b50c35a982d9fb
0xHorror-Dev <horror-dev@bk.ru>
2022-06-10 23:40:48 +0500

    some additional to main menu
    fix UI_Start disabling

--- Assets/Scenes/MainMenu/Menu.cs
+++ Assets/Scenes/MainMenu/Menu.cs
@@ -10,21 +10,40 @@ public class Menu : MonoBehaviour
     GameObject UI_Start;
 
     [SerializeField]
+    Animation UI_StartExitAnimation;
+    
+    [SerializeField]
     InputField NickNameInput;
 
     [SerializeField]
     InputField AddressInput;
 
+    [SerializeField]
+    GameObject Panel;
+
+    bool IsUI_Start = false;
+
     public void OnStartButton()
     {
+        IsUI_Start = !IsUI_Start;
+        Panel.SetActive(false);
         UI_Start.SetActive(true);
+        //Panel.SetActive(true);
+        Debug.Log("::OnStartButton");
+        //ColorBlock m_NewBlock = new ColorBlock();
+        //m_NewBlock.normalColor = Color.black;
+        //StartButton.colors = m_NewBlock;
     }
+
+    private void LoadNextScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     public void OnHostButton()
     {
         NetworkDataHolder.IsHost = true;
         NetworkDataHolder.NickName = NickNameInput.text;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        UI_StartExitAnimation.();
+        Invoke("LoadNextScene", 4.6f);
     }
 
     public void OnConnectButton()
@@ -32,6 +51,13 @@ public class Menu : MonoBehaviour
         NetworkDataHolder.IsHost = false;
         NetworkDataHolder.address = AddressInput.text;
         NetworkDataHolder.NickName = NickNameInput.text;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        UI_StartExitAnimation.Play();
+        Invoke("LoadNextScene", 4.6f);
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
     }
 }
